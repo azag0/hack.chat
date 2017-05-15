88ca94f7c87dfcff9e41aebb6daee3b6e362e874
Jan Hermann <dev@hermann.in>
2017-05-15 22:08:20 +0200

    add basic history logging

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 from collections import defaultdict
+import sqlite3
 import websockets
 import asyncio
 import uvloop
@@ -9,12 +10,26 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 with open('config.json') as f:
     cfg = json.load(f)
 
+db = sqlite3.connect('messages.db', check_same_thread=False)
+db.execute('create table if not exists msgs (channel text, nick text, msg text)')
+
 rooms = defaultdict(set)
 
 
 async def broadcast(obj, channel):
     for client in rooms[channel]:
         await client.send(obj)
+
+
+def get_messages(channel):
+    yield from db.execute(
+        f'select nick, msg from msgs where channel = "{channel}" order by oid'
+    )
+
+
+def log_message(channel, nick, text):
+    db.execute('insert into msgs values (?,?,?)', (channel, nick, text))
+    db.commit()
 
 
 class Client:
@@ -41,6 +56,8 @@ class Client:
             'cmd': 'onlineSet',
             'nicks': [cl.nick for cl in rooms[channel]]
         })
+        for nick, text in get_messages(channel):
+            await self.send({'cmd': 'chat', 'nick': nick, 'text': text})
 
     async def chat(self, text, **kwargs):
         if not self.channel:
@@ -50,6 +67,7 @@ class Client:
             'nick': self.nick,
             'text': text
         }, self.channel)
+        log_message(self.channel, self.nick, text)
 
 
 async def handler(ws, path):
